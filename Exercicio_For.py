ac374d006bcbe5106cb0cdffcfe2fc9413d6d26a
LukDevJobs <luk.devjobs@gmail.com>
2025-07-03 19:37:24 -0300

    Exercicio_For.py
    O for é uma estrutura de controle fundamental em programação que permite iterar sobre uma sequência de valores, executando um bloco de código para cada valor. Ao aprender a utilizar o for, você está desenvolvendo habilidades essenciais para resolver problemas complexos e trabalhar com dados.

--- Exercicio_For.py
+++ Exercicio_For.py
@@ -1,5 +1,24 @@
-num = int(input("Digite um numero:"))
+# Solicita ao usuário que insira dois números inteiros
+inicio = int(input("Digite o início do intervalo: "))
+fim = int(input("Digite o fim do intervalo: "))
 
-for i in range(1,10):
-    print(f"{num} x {i} ={num * i}")
-    
+# Inicializa a soma dos números pares
+soma_pares = 0
+
+# Verifica se o início é maior que o fim e troca os valores se necessário
+if inicio > fim:
+    inicio, fim = fim, inicio
+
+# Verifica se o intervalo tem apenas números ímpares
+if (inicio % 2 != 0) and (fim % 2 != 0):
+    print("Não há números pares no intervalo.")
+else:
+    # Itera sobre todos os números no intervalo
+    for num in range(inicio, fim + 1):
+        # Verifica se o número é par
+        if num % 2 == 0:
+            # Soma o número par à soma total
+            soma_pares += num
+
+    # Exibe a soma dos números pares
+    print("A soma dos números pares no intervalo é:", soma_pares)
