7738efa89f105f88db34f2b672df270ea886d29a
lukasdevjobs1 <luk.devjobs@gmail.com>
2025-09-26 01:59:35 -0300

    feat: Flask CRUD - Sistema de gerenciamento de produtos

--- SUPER-MODULOS/aula3/app.py
+++ SUPER-MODULOS/aula3/app.py
@@ -2,8 +2,8 @@ from flask import Flask
 from config import db, DATABASE_URI 
 from controller import produto_controller
 
-app = flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URI'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
@@ -19,4 +19,6 @@ with app.app_context():
   db.create_all()
   
 if __name__ == "__main__":
+  import os
+  os.environ['FLASK_SKIP_DOTENV'] = '1'
   app.run(debug=True)
