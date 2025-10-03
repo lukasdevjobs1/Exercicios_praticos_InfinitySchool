fe77d32109f8377268979a56b0bcf3bacd0c557f
lukasdevjobs1 <luk.devjobs@gmail.com>
2025-10-03 12:15:10 -0300

    fix: Correct Vercel deployment configuration

--- SUPER-MODULOS/SM-FlaskProject/api/index.py
+++ SUPER-MODULOS/SM-FlaskProject/api/index.py
@@ -1,4 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import app
 
-if __name__ == "__main__":
-    app.run()
+# Para Vercel
+app = app
