338d20706be9e20dd08e1cb068933728cdf91f89
Lukas Gomes <luk.devjobs@gmail.com>
2025-10-02 22:04:49 -0300

    atualizando o projeto
    ainda falta terminar

--- SUPER-MODULOS/aula3/config.py
+++ SUPER-MODULOS/aula3/config.py
@@ -1,9 +1,8 @@
-import os 
-# pip install sqlalchemy
-from  flask_sqlalchemy import SQLAlchemy
+import os
+from flask_sqlalchemy import SQLAlchemy
+# pip install flask_sqlalchemy
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-DATABASE_URI = f'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'database.db')}"
 
 db = SQLAlchemy()
