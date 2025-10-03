6b7d930c668f32a457a476b6c4391d30b5eb1be3
Lukas Gomes <luk.devjobs@gmail.com>
2025-10-03 11:57:38 -0300

    Delete SUPER-MODULOS/aula3 directory

--- SUPER-MODULOS/aula3/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from flask_sqlalchemy import SQLAlchemy
-# pip install flask_sqlalchemy
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'database.db')}"
-
-db = SQLAlchemy()
